fc9707ba23d590f7546607240d525ca9daff971a
Tanishq Gaur <tanishq.gaur07@gmail.com>
2026-01-17 13:38:38 +0530

    fixed few  errors

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,6 +3,9 @@ import { Button } from '@/components/ui/button'
 import { HEATMAP_WIDGET_CONFIG, MARKET_DATA_WIDGET_CONFIG, MARKET_OVERVIEW_WIDGET_CONFIG, TOP_STORIES_WIDGET_CONFIG } from '@/lib/constants'
 import React from 'react'
 
+
+export const dynamic = 'force-dynamic';
+
 function Home() {
   const scriptUrl = `https://s3.tradingview.com/external-embedding/embed-widget-`
   return (
